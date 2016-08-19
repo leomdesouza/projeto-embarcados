b45c0dabfe7d91b95045792c78818aced1b3fe96
leomdesouza <leosouza@kiddolabs.com>
2016-08-18 23:42:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/ABC1010')
+    .get(function(req,res){
+        var responseJson = { 
+            Ligar_lampada: true,
+            Pisca_pisca:  false,
+            pisca_delay: "10 ms"
+        };
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
+app.get('/', function(req, res){
+    res.send('Projeto embarcados!!!');
+});
+
+app.listen(port, function(){
+    console.log('Gul is running my app on PORT: ' + port);
+});
