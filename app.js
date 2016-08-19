8ef822ee912afd4ccaf3dd6ce036cb02c71d4630
leomdesouza <leosouza@kiddolabs.com>
2016-08-19 18:07:44 -0300

    update Procfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = Number(process.env.PORT || 3000);
 
 var bookRouter = express.Router();
 
@@ -23,5 +23,5 @@ app.get('/', function(req, res){
 });
 
 app.listen(port, function(){
-    console.log('Gul is running my app on PORT: ' + port);
+    console.log('Gulp is running my app on PORT: ' + port);
 });
